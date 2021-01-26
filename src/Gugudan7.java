adc894a28938bb1a0526947a9bde54438ecf3060
ssugsangpil <ssugsangpil@172.30.1.19>
2021-01-26 16:00:34 +0900

    self-made loop-in-loop gugudan

---
+++ src/Gugudan7.java
@@ -0,0 +1,12 @@
+
+public class Gugudan7 {
+
+	public static void main(String[] args) {
+		int[] result = new int [9];
+		for(int i = 0; i < result.length; i++) {
+			for(int j = 1; j < 10 ; j++) {
+				System.out.println((i+1) * j );
+			}
+		}
+	}
+}
