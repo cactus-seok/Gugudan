f43be524e4c533a920f1dfcc13ae10d12fcfd53e
ssugsangpil <ssugsangpil@172.30.1.19>
2021-01-26 17:56:33 +0900

    gugudan to finalroad

---
+++ src/Gugudan10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Gugudan10 {
+
+	public static void main(String[] args) {
+		System.out.println("숫자두개눌러 , 넣어서 : ");
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		String[] splitedValue = inputValue.split(",");
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+	
+		for(int i=2; i <= first; i++) {
+			for(int j=1; j <= second; j++) {
+				System.out.println(i * j);
+			}
+		}	
+		}
+	}
---
+++ src/GugudanMain.java
@@ -0,0 +1,9 @@
+
+public class GugudanMain {
+	public static void main(String[] args) {
+		for(int i = 2; i<10; i++) {
+		int[] result = Gugudan9.calculate(i);
+		Gugudan9.print(result);
+		}
+	}
+}
