577cf7e30fb76fbf8093c202db890a9bfcafbe06
ssugsangpil <ssugsangpil@172.30.1.57>
2021-01-26 11:23:48 +0900

    gugudan start

---
+++ src/Gugudan2.java
@@ -0,0 +1,10 @@
+
+public class Gugudan2 {
+	public static void main(String[] args) {
+		int result = 4 * 1;
+		System.out.println(result);
+		
+		result = 4 * 2;
+		System.out.println(result);
+	}
+}
