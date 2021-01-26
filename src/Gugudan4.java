1bcbd0c1fe90e7aea4b1a484cab4009043f4df42
ssugsangpil <ssugsangpil@172.30.1.19>
2021-01-26 14:47:42 +0900

    loop gugudan

---
+++ src/Gugudan4.java
@@ -0,0 +1,22 @@
+
+public class Gugudan4 {
+
+	public static void main(String[] args) {
+		int i = 1 ;
+		while(i<10){
+			System.out.println(6 * i );
+			i = i + 1;
+		}
+
+	
+	
+	for(int j = 1; j < 10; j++) {
+		System.out.println(7 * j);
+		
+	}
+		
+}
+}
+
+
+	
