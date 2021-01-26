41c07001ae13a545431bafdc71398d9aeb5e6239
ssugsangpil <ssugsangpil@172.30.1.19>
2021-01-26 11:54:31 +0900

    gugudan start

--- src/Gugudan2.java
+++ src/Gugudan2.java
@@ -1,10 +1,33 @@
 
 public class Gugudan2 {
 	public static void main(String[] args) {
+		
 		int result = 4 * 1;
 		System.out.println(result);
 		
 		result = 4 * 2;
 		System.out.println(result);
+		
+		result = 4 * 3;
+		System.out.println(result);
+		
+		result = 4 * 4;
+		System.out.println(result);
+		
+		result = 4 * 5;
+		System.out.println(result);
+		
+		result = 4 * 6;
+		System.out.println(result);
+		
+		result = 4 * 7;
+		System.out.println(result);
+		
+		result = 4 * 8;
+		System.out.println(result);
+		
+		result = 4 * 9;
+		System.out.println(result);
+
 	}
 }
